d06ba6c8fe12518d8c2cb32aa03bfaa5610f7968
ScottZ2004 <scottzico2004@gmail.com>
2024-05-10 14:16:03 +0200

    added starting files

---
+++ src/app/homepage.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./homepage.scss";
+
+export default function homepage() {
+  return (
+    <main>
+        <p>Hello world</p>
+    </main>
+  );
+}
