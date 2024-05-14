6de09e550353e71475ea3258862b111c9a17fed9
ScottZ2004 <scottzico2004@gmail.com>
2024-05-14 18:43:10 +0200

    Created files for footer and replaced laravel image

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.scss";
+const Footer = () => {
+    return (
+        <section id="footer">
+            <h1>hello</h1>
+        </section>
+    )
+}
+
+export default Footer;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navigation from "../../components/Navigation/Navigation";
 import Header from "../../components/Header/Header";
 import Projects from "../../components/Projects/Projects";
 import About from "../../components/About/About";
+import Footer from "../../components/Footer/Footer";
 import Experience from "../../components/Experience/Experience"
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <Projects/>
         <About/>
         <Experience/>
+        <Footer/>
     </main>
   );
 }
