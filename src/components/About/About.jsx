1dfc3f215735fbf4f2db033655dad4fff13dfe35
ScottZ2004 <scottzico2004@gmail.com>
2024-05-17 00:37:36 +0200

    the about me section now also translates

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,20 @@
+"use client"
 import portrait from "../../../public/images/portrait.png";
 import "./About.scss";
+import aboutJson from "../../json/sections/about.json"
+import { useLanguage } from "@/context/LanguageContext";
+
 const About = () => {
+    const {language} = useLanguage()
     return (
         <section id="about">
             <div id="left">
-                <h3>About me</h3>
+                <h3>{aboutJson.title[language]}</h3>
                 <p>
-                    Greetings! I'm Scott Zico van den Broek, a dedicated Software Development student at Mediacollege Amsterdam, where my passion for problem-solving meets my penchant for creative expression. Fascinated by the intricacies of both backend and frontend development, I find joy in navigating the complexities of database management while also reveling in the artistry of designing visually striking websites.
+                    {aboutJson.description1[language]}
                     <br/>
                     <br/>
-                    My journey in software development is driven by a dual focus: to deepen my expertise in backend development, exploring advanced techniques and technologies, while simultaneously honing my front-end skills to create seamless and immersive user experiences that captivate and inspire. Join me as I embark on this exhilarating voyage through the ever-evolving landscape of software development, fueled by curiosity, creativity, and an unwavering commitment to excellence.
+                    {aboutJson.description2[language]}
 
                 </p>
             </div>
