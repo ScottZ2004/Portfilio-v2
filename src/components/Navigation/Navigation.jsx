1b85857e0bcda2dce7f8332e72cb51c43e62c9e2
ScottZ2004 <scottzico2004@gmail.com>
2024-05-17 13:30:44 +0200

    the navigation now also being translated

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,17 @@
+"use client";
 import "./Navigation.scss";
 import logo from "../../../public/images/logo/logo_scott_1.png";
+import naviagtionJson from "../../json/sections/navigation.json";
+import { useLanguage } from "@/context/LanguageContext";
 function Navigation() {
+    const {language} = useLanguage()
     return (
         <header>
             <a href="#home"><img src={logo.src} alt="logo"/></a>
             <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#experience">Experience</a></li>
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                <li><a href="mailto:scottzico2004@gmail.com">Let's get in touch</a></li>
+                {naviagtionJson.map((navigation, index) => {
+                    return( <li key={index}><a href={navigation.link}>{navigation.title[language]}</a></li>)
+                })}
             </ul>
         </header>
     );
