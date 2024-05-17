b72afddd87c13e3b2cd225f5a67de3879910f98c
ScottZ2004 <scottzico2004@gmail.com>
2024-05-17 13:20:39 +0200

    the experience card now also translate

--- src/components/Experience/ExperienceCard/ExperienceCard.jsx
+++ src/components/Experience/ExperienceCard/ExperienceCard.jsx
@@ -1,12 +1,15 @@
+"use client";
+import { useLanguage } from "@/context/LanguageContext";
 import "./ExperienceCard.scss";
 const ExperienceCard = (props) => {
+    const {language} = useLanguage()
     return (
         <div className="experienceCard">
             <img src={props.experience.image} alt="Experience"/>
             <span></span>
             <div>
                 <h3>{props.experience.title}</h3>
-                <p>{props.experience.description}</p>
+                <p>{props.experience.description[language]}</p>
             </div>
         </div>
     )
